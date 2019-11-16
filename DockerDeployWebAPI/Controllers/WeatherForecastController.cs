62b80472c2738708cf8f0bb8441f92958a9a24d2
Shi Hao Wang <st890409@hotmail.com>
2019-11-16 20:00:12 +0800

    Update WeatherForecastController.cs

--- DockerDeployWebAPI/Controllers/WeatherForecastController.cs
+++ DockerDeployWebAPI/Controllers/WeatherForecastController.cs
@@ -27,7 +27,7 @@ namespace DockerDeployWebAPI.Controllers
         public IEnumerable<WeatherForecast> Get()
         {
             var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 2).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
